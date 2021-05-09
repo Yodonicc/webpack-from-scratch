481331275e68ff87a66f04967aa05ad9cc4c6f46
yodofaither <yodofaither@outlook.com>
2021-05-09 16:43:51 +0800

    add: url-loader, css-loader, style-loader

--- webpack-demo/webpack.config.js
+++ webpack-demo/webpack.config.js
@@ -4,6 +4,44 @@ module.exports = {
     entry: './app/index.js',
     output: {
         path: path.resolve(__dirname, 'build'),
-        filename: 'bundle.js'
+        filename: 'bundle.js',
+        publicPath: 'build/'
+    },
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                use: 'babel-loader',
+                exclude: /node_modules/
+            },
+            {
+                test: /\.(png|jpe?g|gif|svg)(\?.*)?$/,
+                use: [
+                    {
+                        loader: 'url-loader',
+                        options: {
+                            esModule: false,
+                            limit: 10000,
+                            name: 'images/[name].[hash].[ext]'
+                        }
+                    },
+                    
+                ]
+            },
+            {
+                test: /\.css$/,
+                use: [
+                    'style-loader',
+                    {
+                        loader: 'css-loader',
+                        options: {
+                            modules: true
+                        }
+                    }
+                    
+                ],
+            },
+        ]
+        
     }
 }
